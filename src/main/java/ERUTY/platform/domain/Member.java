9a764adbb088c6ee520b8fcdc73171e200a376d8
INUK-ai <arheon2469@gmail.com>
2023-01-03 02:59:17 +0900

    Sample Member and Repository

--- src/main/java/ERUTY/platform/domain/Member.java
+++ src/main/java/ERUTY/platform/domain/Member.java
@@ -1,7 +1,27 @@
 package ERUTY.platform.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "member")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+
+    @Id
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    @Builder
+    public Member(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
