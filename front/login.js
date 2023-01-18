a4b557749fb4b3f5be4b12b87017c8145b0205f3
INUK-ai <arheon2469@gmail.com>
2023-01-18 22:23:04 +0900

    Merge 수정

---
+++ front/login.js
@@ -0,0 +1,112 @@
+// 유효성 검사
+function checkFindPassword() {
+  if (findPasswordform.exampleInputEmail1.value == "") {
+    $("#findPWCheckMessage").show();
+    return false;
+  }
+  $("#findPWCheckMessage").hide();
+  return true;
+}
+
+function checkLogin() {
+  if (
+    loginform.exampleInputEmail1.value == "" ||
+    loginform.exampleInputPassword1.value == ""
+  ) {
+    $("#loginMessage").show();
+    return false;
+  }
+  $("#loginMessage").hide();
+  return true;
+}
+
+function checkSignUp() {
+  if (signUpform.exampleInputEmail1.value == "") {
+    $("#signUpEmailMessage").show();
+    return false;
+  }
+  $("#signUpEmailMessage").hide();
+
+  if (signUpform.exampleInputPassword1.value == "") {
+    $("#signUpPWMessage").show();
+    return false;
+  }
+  $("#signUpPWMessage").hide();
+
+  if (
+    signUpform.exampleInputPassword1.value !=
+    signUpform.exampleInputPassword2.value
+  ) {
+    $("#signUpPWDMessage").show();
+    return false;
+  }
+  $("#signUpPWDMessage").hide();
+
+  if (
+    signUpform.exampleCheck4.checked == false ||
+    signUpform.exampleCheck3.checked == false ||
+    signUpform.exampleCheck2.checked == false ||
+    signUpform.exampleCheck5.checked == false
+  ) {
+    $("#signUpTermMessage").show();
+    return false;
+  }
+  $("#signUpTermMessage").hide();
+  return true;
+}
+
+function checkChangePassword() {
+  if (
+    changePasswordform.exampleInputEmail1.value == "" ||
+    changePasswordform.exampleInputPassword1.value == ""
+  ) {
+    $("#changeMessage").show();
+    return false;
+  }
+  $("#changeMessage").hide();
+
+  if (changePasswordform.exampleInputPassword2.value == "") {
+    $("#changePWMessage").show();
+    return false;
+  }
+  $("#changePWMessage").hide();
+
+  if (
+    changePasswordform.exampleInputPassword2.value !=
+    changePasswordform.exampleInputPassword3.value
+  ) {
+    $("#changePWDMessage").show();
+    return false;
+  }
+  $("#changePWDMessage").hide();
+  console.log("come in");
+  return false;
+}
+// 전체선택
+
+$("#allcheck").on("click", function () {
+  var checked = $(this).is(":checked");
+  if (checked) {
+    $(this)
+      .closest(".agreement-container")
+      .find(".form-check-input")
+      .prop("checked", true);
+  } else {
+    $(this)
+      .closest(".agreement-container")
+      .find(".form-check-input")
+      .prop("checked", false);
+  }
+  console.log($(this).closest(".agreement-container"));
+});
+$(".agreement-container .form-check-input").on("click", function () {
+  var chkGroup = $(this).closest(".agreement-container").find(".check-group");
+  var chkGroup_cnt = chkGroup.length;
+  checked_cnt = $(".check-group .form-check-input:checked").length;
+
+  if (checked_cnt < chkGroup_cnt) {
+    $("#allcheck").prop("checked", false);
+  } else if (checked_cnt == chkGroup_cnt) {
+    $("#allcheck").prop("checked", true);
+  }
+});
---
+++ front/uploadRendering.js
@@ -0,0 +1,52 @@
+const myCanvas = document.getElementById("server-3d");
+const loader = new THREE.OBJLoader();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+const renderer = new THREE.WebGLRenderer({
+  canvas: myCanvas,
+  alpha: true,
+});
+renderer.setSize(335, 335);
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.rotateSpeed = 1.0; // 마우스로 카메라를 회전시킬 속도입니다. 기본값(Float)은 1입니다.
+controls.zoomSpeed = 1.2; // 마우스 휠로 카메라를 줌 시키는 속도 입니다. 기본값(Float)은 1입니다.
+controls.panSpeed = 0.8; // 패닝 속도 입니다. 기본값(Float)은 1입니다.
+controls.minDistance = 5; // 마우스 휠로 카메라 거리 조작시 최소 값. 기본값(Float)은 0 입니다.
+controls.maxDistance = 100; // 마우스 휠로 카메라 거리 조작시 최대 값. 기본값(Float)은 무제한 입니다.
+
+const object = new THREE.Object3D();
+loader.load(
+  "./cat.obj",
+  function (objectload) {
+    object.add(objectload);
+    scene.add(object);
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+object.add(cube);
+
+function animate() {
+  requestAnimationFrame(animate);
+  object.rotation.z -= 0.01;
+  object.rotation.y -= 0.01;
+  renderer.render(scene, camera);
+  controls.update();
+}
+
+animate();
