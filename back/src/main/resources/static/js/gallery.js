689c2a1fbc9522c3642aa66878246371fbb98551
Junseo <monstazo911@gmail.com>
2023-02-18 12:01:35 +0900

    main page animation + list banner

--- back/src/main/resources/static/js/gallery.js
+++ back/src/main/resources/static/js/gallery.js
@@ -1,15 +1,15 @@
 const sorting_list = document.querySelectorAll(".gallery-order");
 
-function changeSelect(e) {
-  sorting_list.forEach((element) => {
-    if (element == e) {
-      element.classList.add("order-selected");
-    } else {
-      element.classList.remove("order-selected");
-    }
-  });
-}
+// function changeSelect(e) {
+//   sorting_list.forEach((element) => {
+//     if (element == e) {
+//       element.classList.add("order-selected");
+//     } else {
+//       element.classList.remove("order-selected");
+//     }
+//   });
+// }
 
-sorting_list[0].addEventListener("click", () => changeSelect(sorting_list[0]));
-sorting_list[1].addEventListener("click", () => changeSelect(sorting_list[1]));
-sorting_list[2].addEventListener("click", () => changeSelect(sorting_list[2]));
+// sorting_list[0].addEventListener("click", () => changeSelect(sorting_list[0]));
+// sorting_list[1].addEventListener("click", () => changeSelect(sorting_list[1]));
+// sorting_list[2].addEventListener("click", () => changeSelect(sorting_list[2]));
--- back/src/main/resources/static/js/main.js
+++ back/src/main/resources/static/js/main.js
@@ -1,3 +1,16 @@
 window.addEventListener("onclick", function (e) {
   console.log(e.target);
 });
+
+// window.addEventListener("scroll", () => {
+//   //스크롤을 할 때마다 로그로 현재 스크롤의 위치가 찍혀나온다.
+//   console.log(window.scrollX, window.scrollY);
+//   const bg = document.querySelector(".bg-ani");
+//   if (window.scrollY > 1385) {
+//     bg.classList.remove("first-bg");
+//     bg.classList.add("second-bg");
+//   } else {
+//     bg.classList.remove("second-bg");
+//     bg.classList.add("first-bg");
+//   }
+// });
