765cff45475f9faa639a525d69f048862e767d74
INUK-ai <arheon2469@gmail.com>
2023-02-20 13:21:31 +0900

    Ajax #1 - Responsebody

--- back/src/main/java/ERUTY/platform/controller/ItemController.java
+++ back/src/main/java/ERUTY/platform/controller/ItemController.java
@@ -12,10 +12,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -136,6 +133,7 @@ public class ItemController {
     }
 
     @GetMapping("getLikeById")
+    @ResponseBody
     public Boolean liked(@RequestParam String itemId, HttpSession session) {
         String memberId = (String) session.getAttribute("loginId");
 
